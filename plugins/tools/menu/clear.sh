e5814530a2e65ce5d9fb1da087284c42b65dbdd5
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2023-10-28 01:10:29 +0700

    CLEAR TRASH 🗑️

--- plugins/tools/menu/clear.sh
+++ plugins/tools/menu/clear.sh
@@ -7,7 +7,7 @@ get_ram_status() {
     
     # Membersihkan cache RAM
     sync
-    echo 1 > /proc/sys/vm/drop_caches
+    echo 3 > /proc/sys/vm/drop_caches
     
     free_ram_after=$(free -m | awk 'NR==2{print $7}')
     erased_ram=$((free_ram_before - free_ram_after))
