a25753b6b70e5adc3c7b61015b028805c8b294b6
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2023-11-01 07:58:50 +0700

    5GB

---
+++ plugins/tools/testbw/5GB.sh
@@ -0,0 +1,227 @@
+#!/bin/bash
+
+echo"
+░█████╗░██████╗░░█████╗░████████╗
+██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝
+██║░░╚═╝██████╔╝██║░░██║░░░██║░░░
+██║░░██╗██╔══██╗██║░░██║░░░██║░░░
+╚█████╔╝██║░░██║╚█████╔╝░░░██║░░░
+░╚════╝░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░"
+echo "BANDWITH LIMIT TEST"
+echo "5GB TEST"
+sleep 5
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+wget https://github.com/EdyDevz/BW/raw/main/BW && rm BW
+sleep 10
+# READ AUTH
+if [ -f "/root/TgBotWRT/AUTH" ]; then
+    TOKEN=$(head -n 1 /root/TgBotWRT/AUTH)
+    ID=$(tail -n 1 /root/TgBotWRT/AUTH)
+else
+    echo "Berkas AUTH tidak ditemukan."
+    exit 1
+fi
+
+MSG "5GB TEST SUCCESSFUL✅"
+
+# Kirim pesan notifikasi ke bot Telegram
+curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" -d "chat_id=$ID" -d "text=$MSG"
+exit
