1aeb411672556c11e646e74c0338f0b34e8c20d6
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2023-12-04 21:50:26 +0700

    SCRIPT MENU ⚙️

--- menu.sh
+++ menu.sh
@@ -2,22 +2,21 @@
 
 # Asosiasi antara judul dan URL
 declare -A SCRIPTS
-SCRIPTS["system"]="https://tgbotwrt.titit.tech/system.sh"
-SCRIPTS["speedtest"]="https://tgbotwrt.titit.tech/speedtest.sh"
-SCRIPTS["ping"]="https://tgbotwrt.titit.tech/ping.sh"
-SCRIPTS["restart"]="https://tgbotwrt.titit.tech/restart.sh"
-SCRIPTS["reboot"]="https://tgbotwrt.titit.tech/reboot.sh"
-SCRIPTS["shutdown"]="https://tgbotwrt.titit.tech/shutdown.sh"
-SCRIPTS["ocstart"]="https://tgbotwrt.titit.tech/ocstart.sh"
-SCRIPTS["ocrestart"]="https://tgbotwrt.titit.tech/ocrestart.sh"
-SCRIPTS["ocstop"]="https://tgbotwrt.titit.tech/ocstop.sh"
-SCRIPTS["clear"]="https://tgbotwrt.titit.tech/clear.sh"
-SCRIPTS["vnstat"]="https://tgbotwrt.titit.tech/vnstat.sh"
-SCRIPTS["vnstat-d"]="https://tgbotwrt.titit.tech/vnstat-d.sh"
-SCRIPTS["vnstat-m"]="https://tgbotwrt.titit.tech/vnstat-m.sh"
-SCRIPTS["vnstat-y"]="https://tgbotwrt.titit.tech/vnstat-y.sh"
-SCRIPTS["updater"]="https://tgbotwrt.titit.tech/updater.sh"
-SCRIPTS["stop"]="https://tgbotwrt.titit.tech/stop.sh"
+SCRIPTS["system"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/system.sh"
+SCRIPTS["speedtest"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/speedtest.sh"
+SCRIPTS["ping"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/ping.sh"
+SCRIPTS["restart"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/restart.sh"
+SCRIPTS["reboot"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/reboot.sh"
+SCRIPTS["shutdown"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/shutdown.sh"
+SCRIPTS["ocstart"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/ocstart.sh"
+SCRIPTS["ocrestart"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/ocrestart.sh"
+SCRIPTS["ocstop"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/ocstop.sh"
+SCRIPTS["clear"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/clear.sh"
+SCRIPTS["vnstat"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/vnstat.sh"
+SCRIPTS["vnstat-d"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/vnstat-d.sh"
+SCRIPTS["vnstat-m"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/vnstat-m.sh"
+SCRIPTS["vnstat-y"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/vnstat-y.sh"
+SCRIPTS["stop"]="https://raw.githubusercontent.com/EdyDevz/TgBotWRT/OFFLINE/TOOLS/stop.sh"
 # Tambahkan judul dan URL lain sesuai kebutuhan
 
 # Memeriksa apakah argumen yang diberikan adalah judul yang valid
