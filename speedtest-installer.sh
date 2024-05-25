5bee2cd51eb7b6d9479eca7b7a0ac4dc078c647d
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2024-05-25 18:45:24 +0700

    SPEEDTEST INSTALLER 🚀

--- speedtest-installer.sh
+++ speedtest-installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --no-check-certificate https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-aarch64.tgz -O /tmp/speedtest.tgz && tar -xzvf /tmp/speedtest.tgz -C /usr/bin/ && chmod +x /usr/bin/speedtest && clear && speedtest
+opkg update && wget --no-check-certificate https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-linux-aarch64.tgz -O /tmp/speedtest.tgz && tar -xzvf /tmp/speedtest.tgz -C /usr/bin/ && chmod +x /usr/bin/speedtest && clear && speedtest
