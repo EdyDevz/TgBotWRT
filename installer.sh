1a270566c9db0bfd2d4db3bcf7f4b01ca0e80907
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2024-05-25 18:22:33 +0700

    INSTALLER 🚀

--- installer.sh
+++ installer.sh
@@ -5,29 +5,41 @@ echo "INSTALLING GIT"
 sleep 2
 opkg install git
 opkg install git-http
+echo "SUCCESS"
+sleep 2
 clear
 echo "INSTALLING PYTHON"
 sleep 2
 opkg install python3
 opkg install python3-pip
+echo "SUCCESS"
+sleep 2
 clear
 echo "INSTALLING JQ"
 sleep 2
 opkg install jq
+echo "SUCCESS"
+sleep 2
 clear
 echo "INSTALLING SYSSTAT"
 sleep 2
 opkg install sysstat
+echo "SUCCESS"
+sleep 2
 clear
 echo "INSTALLING NANO"
 sleep 2
 opkg install nano
+echo "SUCCESS"
+sleep 2
 clear
 echo "INSTALLING TMUX"
 sleep 2
 opkg install tmux
+echo "SUCCESS"
+sleep 2
 clear
-echo "SETUP TOOLS ...."
+echo "INSTALLING TOOLS ...."
 sleep 2
 pip3 install telepot requests python-telegram-bot
 opkg list-installed | grep python3
@@ -41,6 +53,8 @@ chmod +x /etc/init.d/edy
 chmod +x /usr/bin/edy.py
 chmod +x /root/TgBotWRT/*
 chmod +x /root/TgBotWRT/TOOLS/*
+echo "SUCCESS"
+sleep 2
 clear
 echo "JANGAN LUPA ISI BOT TOKEN DAN ID TELEGRAM YA ANJING"
 sleep 10
@@ -50,7 +64,8 @@ clear
 nano /root/TgBotWRT/AUTH
 echo "PLEASE WAIT....."
 sleep 5
-echo "INSTALLING TOOLS ....."
+clear
+echo "SETUP TOOLS ....."
 sleep 5
 clear
 echo "
@@ -64,4 +79,4 @@ echo "
 ╚════════════════════════════════════════════════════════╝"
 echo "[+] THAKS FOR USE MY TOOLS & SUPPORT ME :)"
 service edy enable
-launchbot
+service edy start
