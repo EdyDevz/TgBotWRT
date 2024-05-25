fbbc7563a8382901e1cdc902ade9e3b53d9e1efb
Edy Developer <144365814+EdyDevz@users.noreply.github.com>
2024-05-25 18:42:11 +0700

    INSTALLER 🚀

--- installer.sh
+++ installer.sh
@@ -1,44 +1,50 @@
 #!/bin/bash
 
+echo "UPDATING REPOSITORY"
+sleep 2
+opkg update
+clear
+echo "SUCCESS"
+sleep 2
 cd
 echo "INSTALLING GIT"
 sleep 2
 opkg install git
 opkg install git-http
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING PYTHON"
 sleep 2
 opkg install python3
 opkg install python3-pip
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING JQ"
 sleep 2
 opkg install jq
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING SYSSTAT"
 sleep 2
 opkg install sysstat
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING NANO"
 sleep 2
 opkg install nano
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING TMUX"
 sleep 2
 opkg install tmux
+clear
 echo "SUCCESS"
 sleep 2
-clear
 echo "INSTALLING TOOLS ...."
 sleep 2
 pip3 install telepot requests python-telegram-bot
@@ -47,15 +53,13 @@ pip3 list
 git clone -b SCRIPT https://github.com/EdyDevz/TgBotWRT
 mv /root/TgBotWRT/edy /etc/init.d/
 mv /root/TgBotWRT/edy.py /usr/bin/
-mv /root/TgBotWRT/launchbot /usr/bin/
-chmod +x /usr/bin/launchbot
 chmod +x /etc/init.d/edy
 chmod +x /usr/bin/edy.py
 chmod +x /root/TgBotWRT/*
 chmod +x /root/TgBotWRT/TOOLS/*
-echo "SUCCESS"
-sleep 2
 clear
+echo "SUCCESS"
+sleep 5
 echo "JANGAN LUPA ISI BOT TOKEN DAN ID TELEGRAM YA ANJING"
 sleep 10
 echo "TUTORIAL DAH LENGKAP JANGAN BANYAK TANYA YA ANJING"
